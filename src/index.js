c062a65139e69e763923fba3eaf0eacaaafae9c3
Your Name <you@example.com>
2017-08-21 10:45:42 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BroswerRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.render(
+  <BroswerRouter><App /></BroswerRouter>,
+  document.getElementById('root')
+);
